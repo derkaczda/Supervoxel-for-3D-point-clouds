34e9f3f38d0fa815d1f49d3dadfa9fc9a7afae7c
Daniel Derkacz-Bogner <mail@daniel-derkacz.de>
2022-10-13 15:07:24 +0200

    Changes in visualize script

--- visualize.py
+++ visualize.py
@@ -2,13 +2,14 @@ import open3d as o3d
 import argparse
 import os.path as osp
 import numpy as np
+import os
 
 def get_supervoxel_mapping(path):
     return np.loadtxt(path).astype(int)
 
 
 def main(args):
-    pcd = o3d.io.read_point_cloud(osp.join(args.xyz_dir, f"{args.filename}..xyz"), format="xyzrgb")
+    pcd = o3d.io.read_point_cloud(osp.join(args.xyz_dir, f"{args.filename}.xyz"), format="xyzrgb")
     mapping = get_supervoxel_mapping(osp.join(args.supervox_dir, f"{args.filename}_out_labels.txt"))
     supervoxel_ids = np.unique(mapping)
 
@@ -26,4 +27,5 @@ if __name__ == "__main__":
     parser.add_argument('--dest')
     parser.add_argument('--filename', help='without extension')
     args = parser.parse_args()
+    os.makedirs(args.dest, exist_ok=True)
     main(args)
