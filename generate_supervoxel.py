c556c23a5a43264d960a0503d11742400ebc4f4c
Daniel Derkacz <mail@daniel-derkacz.de>
2022-12-13 12:23:33 +0100

    updated generate supervoxel script

--- generate_supervoxel.py
+++ generate_supervoxel.py
@@ -10,14 +10,14 @@ os.umask(0000)
 from multiprocessing import Pool, cpu_count
 from functools import partial
 
-def generate_supervoxel(xyz_file, dest):
+def generate_supervoxel(xyz_file, dest, resolution):
     filename = xyz_file.split('/')[-1].split('.')[0]
     print(f"Processing file {filename}")
     if osp.exists(osp.join(dest, f"{filename}_out.xyz")):
         print("File exists, skipping ...")
         return
     sys.stdout.flush()
-    os.system(f"./build/supervoxel {osp.abspath(xyz_file)}")
+    os.system(f"./build/supervoxel {osp.abspath(xyz_file)} {resolution}")
     shutil.move(f"{filename}_out.xyz", osp.join(dest, f"{filename}_out.xyz"))
     shutil.move(f"{filename}_out_vccs.xyz", osp.join(dest, f"{filename}_out_vccs.xyz"))
     shutil.move(f"{filename}_out_vccs_knn.xyz", osp.join(dest, f"{filename}_out_vccs_knn.xyz"))
@@ -30,7 +30,7 @@ def main(args):
     xyz_files = glob.glob(osp.join(args.dir, "*.xyz"))
     with Pool(args.n_proc) as pool:
         pool.map(
-            partial(generate_supervoxel, dest=args.dest),
+            partial(generate_supervoxel, dest=args.dest, resolution=args.res),
             xyz_files
         )
 
@@ -41,6 +41,7 @@ if __name__ == "__main__":
     parser.add_argument("--dir")
     parser.add_argument("--dest")
     parser.add_argument("--n_proc", default=cpu_count(), type=int)
+    parser.add_argument("--res", type=float)
     args = parser.parse_args()
 
     os.makedirs(args.dest, exist_ok=True)
